b9749c44911fe2f01125a04c5989c2ed4fc4e4ca
andersondln <59345168+andersondln@users.noreply.github.com>
2021-03-06 11:32:59 -0300

    Upload first version
    Subindo a primeira versão.

---
+++ csvtojson_andani.js
@@ -0,0 +1,43 @@
+function CSVtoJSON(csv, filter){
+
+    let final = [];
+    let headerPosition = [];
+    let content = csv.split('\n');
+    let headerLines = content[0].split(',');
+    let errorStyle = 'color: white; background-color:blue; font-weight: bold; font-size:15px;';
+    if(filter != null && filter != '' && Array.isArray(filter)){
+     headerLines.forEach((me, meindex)=>{
+         if(filter.includes(me))
+         headerPosition.push(me+':'+meindex);
+     });
+     if(headerPosition.length == 0)
+     console.log('%c [CSVtoJSON]\nBy Anderson Daniel \n'
+     +' ::: Filtro não encontrado. :::', errorStyle);
+     else if(headerPosition.length < filter.length)
+     console.log('%c [CSVtoJSON] \n by Anderson Daniel'
+     +'\n ::: Um ou mais filtros não foram encontrados. :::',errorStyle);
+    }
+
+    content.forEach((me, meindex)=>{
+        let lines = me.split(',');
+        let obj = {}
+        if(meindex > 0){
+             if(headerPosition.length > 0){
+                     headerPosition.forEach(e=>{
+                     let esplit = e.split(':');
+                     obj[esplit[0]] = lines[esplit[1]];
+              
+                 });
+                 final.push(obj);
+             }else{
+                headerLines.forEach((e, eindex)=>{
+                   obj[e] = lines[eindex];
+                });
+                final.push(obj);
+             }
+        }
+    }) 
+   
+    return final;
+
+ }
